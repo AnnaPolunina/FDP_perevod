9dc253e762ac589b130af47212c2bdd2ba013248
AtretCM <atret1988@gmail.com>
2023-03-28 20:30:04 +0300

    CRUD

--- perevod_app/views.py
+++ perevod_app/views.py
@@ -1,6 +1,36 @@
 from openpyxl import load_workbook
 from django.shortcuts import render
 from .models import Translation
+from django.views import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Translation
+
+
+class WordsBaseView(View):
+    model = Translation
+    fields = '__all__'
+    success_url = reverse_lazy('all')
+
+class WordsListView(WordsBaseView, ListView):
+    """Список всех слов"""
+
+class WordsDetailView(WordsBaseView, DetailView):
+    """Определенная пара слов"""
+
+class WordsCreateView(WordsBaseView, CreateView):
+    """Создание новой пары слов"""
+
+class WordsUpdateView(WordsBaseView, UpdateView):
+    """Редактирование пары слов"""
+
+class WordsDeleteView(WordsBaseView, DeleteView):
+    """Удаление пары слов"""
+
+'''class SearchWord(WordsBaseView):
+    def get(request):'''
 
 def search_word(request):
     # загрузка файла Excel
@@ -18,15 +48,20 @@ def search_word(request):
 
 
     result = 'Слово не знайдено'
-    return render(request, 'search_word.html', {'result': result})
+    return render(request, 'perevod_app/search_word.html', {'result': result})
 
 def add_words_from_exel():
     '''сохранение всех записей с exel в БД'''
     wb = load_workbook(filename='translations.xlsx')
     sheet = wb['Sheet1']
     for row in sheet.iter_rows(values_only=True):
-        trans_bd = Translation()
-        trans_bd.en=row[0]
-        trans_bd.uk=row[1]
-        print(row)
-        trans_bd.save()
+        try:
+            trans_bd = Translation()
+            trans_bd.en=row[0]
+            trans_bd.uk=row[1]
+            trans_bd.save()
+        except:
+            continue
+
+def add_words_from_exel_in_DB():
+    '''сохранение всех записей с БД в еxel'''
